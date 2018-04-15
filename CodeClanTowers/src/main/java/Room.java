03cfd26f9ea89a355cc4d73b6bcd342a48d48489
Mike-Heneghan <m.heneghan@live.co.uk>
2018-04-15 18:36:36 +0100

    Abstract Room class created.

---
+++ CodeClanTowers/src/main/java/Room.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public abstract class Room {
+    private Integer capacity;
+    private ArrayList<Guest> guests;
+
+    public Room(Integer capacity){
+        this.capacity = capacity;
+        this.guests = new ArrayList<>();
+
+    }
+
+    public Integer getCapacity(){
+        return capacity;
+    }
+
+    public ArrayList<Guest> getGuests() {
+        return guests;
+    }
+}
